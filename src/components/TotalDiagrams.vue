0394649b89752cfe4e7938a8e2cf3ee5ebbf8560
Iordanis Paschalidis <iordanispaschalidis@Iordaniss-MacBook-Pro.local>
2020-05-22 13:13:29 +0100

    fixing more ui

--- src/components/TotalDiagrams.vue
+++ src/components/TotalDiagrams.vue
@@ -1,29 +1,53 @@
 <template>
 
   <div id="diagrams" class="float-middle box">
-    <total-diagram 
-      v-bind:countries="[ sdeaths[0].country, sdeaths[1].country, sdeaths[2].country, sdeaths[3].country, sdeaths[4].country, sdeaths[5].country]" 
-      v-bind:deathsNumber="[ sdeaths[0].deaths, sdeaths[1].deaths, sdeaths[2].deaths, sdeaths[3].deaths, sdeaths[4].deaths, sdeaths[5].deaths]"
-      v-bind:typeOfDiagram= "'Number of deaths'"
-    />
+    <div>
+      <div class="w3-container w3-content newsStand">
+        <div class="w3-panel w3-white w3-card w3-display-container">
+          <total-diagram 
+            v-bind:countries="[ sdeaths[0].country, sdeaths[1].country, sdeaths[2].country, sdeaths[3].country, sdeaths[4].country, sdeaths[5].country]" 
+            v-bind:deathsNumber="[ sdeaths[0].deaths, sdeaths[1].deaths, sdeaths[2].deaths, sdeaths[3].deaths, sdeaths[4].deaths, sdeaths[5].deaths]"
+            v-bind:typeOfDiagram= "'Number of deaths'"
+          />
+        </div>
+      </div>
+    </div>
+    <div>
+      <div class="w3-container w3-content newsStand">
+        <div class="w3-panel w3-white w3-card w3-display-container">
+          <total-diagram 
+            v-bind:countries="[ scases[0].country, scases[1].country, scases[2].country, scases[3].country, scases[4].country, scases[5].country]" 
+            v-bind:deathsNumber="[ scases[0].cases, scases[1].cases, scases[2].cases, scases[3].cases, scases[4].cases, scases[5].cases]"
+            v-bind:typeOfDiagram= "'Number of Cases'"
+          />
+        </div>
+      </div>
+    </div>
 
-    <total-diagram 
-      v-bind:countries="[ scases[0].country, scases[1].country, scases[2].country, scases[3].country, scases[4].country, scases[5].country]" 
-      v-bind:deathsNumber="[ scases[0].cases, scases[1].cases, scases[2].cases, scases[3].cases, scases[4].cases, scases[5].cases]"
-      v-bind:typeOfDiagram= "'Number of Cases'"
-    />
+     <div>
+      <div class="w3-container w3-content newsStand">
+        <div class="w3-panel w3-white w3-card w3-display-container">
+          <total-diagram 
+            v-bind:countries="[ active[0].country, active[1].country, active[2].country, active[3].country, active[4].country, active[5].country]" 
+            v-bind:deathsNumber="[ active[0].active, active[1].active, active[2].active, active[3].active, active[4].active, active[5].active]"
+            v-bind:typeOfDiagram= "'Number of Active'"
+          />
+        </div>
+      </div>
+    </div>
 
-    <total-diagram 
-      v-bind:countries="[ active[0].country, active[1].country, active[2].country, active[3].country, active[4].country, active[5].country]" 
-      v-bind:deathsNumber="[ active[0].active, active[1].active, active[2].active, active[3].active, active[4].active, active[5].active]"
-      v-bind:typeOfDiagram= "'Number of Active'"
-    />
+    <div>
+      <div class="w3-container w3-content newsStand">
+        <div class="w3-panel w3-white w3-card w3-display-container">
+          <total-diagram 
+            v-bind:countries="[ recovered[0].country, recovered[1].country, recovered[2].country, recovered[3].country, recovered[4].country, recovered[5].country]" 
+            v-bind:deathsNumber="[ recovered[0].recovered, recovered[1].recovered, recovered[2].recovered, recovered[3].recovered, recovered[4].recovered, recovered[5].recovered]"
+            v-bind:typeOfDiagram= "'Number of Recovered'"
+          />
+        </div>
+      </div>
+    </div>
 
-    <total-diagram 
-      v-bind:countries="[ recovered[0].country, recovered[1].country, recovered[2].country, recovered[3].country, recovered[4].country, recovered[5].country]" 
-      v-bind:deathsNumber="[ recovered[0].recovered, recovered[1].recovered, recovered[2].recovered, recovered[3].recovered, recovered[4].recovered, recovered[5].recovered]"
-      v-bind:typeOfDiagram= "'Number of Recovered'"
-    />
   </div>
 
 </template>
@@ -97,6 +121,11 @@
   }
   .box>* {
     flex: 1 1 80px;
+  }
+
+  .newsStand{
+    width: 670px;
+    height: 400px;
   }
 
 </style>
