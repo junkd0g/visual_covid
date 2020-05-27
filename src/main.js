138c8346db70faa786c7cdb7e9a186017449c5ef
Iordanis Paschalidis <iordanis@ipaschalidis.com>
2020-05-27 12:35:34 +0100

    removing stuff

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import App from './App.vue'
-import axios from 'axios'
 import router from './router'
 
 import 'bootstrap/dist/css/bootstrap.css'
@@ -8,30 +7,14 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'nprogress/nprogress.css';
 import VueMeta from 'vue-meta'
-import VueCookie from 'vue-cookie';
 
-Vue.use(VueCookie);
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
 Vue.use(VueMeta)
 
 Vue.config.productionTip = false
 
-// From some method in one of your Vue components
-// This will set a cookie with the name 'test' and the value 'Hello world!' that expires in one day
- 
-// To get the value of a cookie use
 
-Vue.use({
-  install (Vue) {
-    Vue.prototype.$api = axios.create({
-      baseURL: 'http://127.0.0.1::9080',
-      headers: {
-        'Content-Type' : 'application/json'
-      }
-    })
-  }
-})
 
 new Vue({
   router,
