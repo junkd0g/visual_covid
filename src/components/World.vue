0e3525ec54365eea993cde2be029677f25c63a15
Iordanis Paschalidis <iordanis@ipaschalidis.com>
2020-06-02 14:15:12 +0100

    fixing mobile

--- src/components/World.vue
+++ src/components/World.vue
@@ -5,8 +5,8 @@
         <mdb-line-chart
           :data="lineChartData"
           :options="lineChartOptions"
-          :width="1000"
-          :height="500"
+          :width="dimension.width"
+          :height="dimension.height"
         />
     </mdb-container>
   </div>
@@ -16,6 +16,7 @@
 <script>
   import { mdbLineChart, mdbContainer } from "mdbvue";
   import axios from 'axios'
+  import { isMobile } from 'mobile-device-detect';
   import FEData from '../lib/FEData.js';
   var fedata= new FEData();
 
@@ -25,6 +26,13 @@
       mdbLineChart,
       mdbContainer
     },
+    created() {
+        window.addEventListener('resize', this.handleResize);
+        this.handleResize();
+    },
+    destroyed() {
+        window.removeEventListener('resize', this.handleResize);
+    },
     methods: {
         requestWorld(){
           axios.get('http://localhost:9080/api/world')
@@ -33,6 +41,15 @@
           .catch(function (error) {   
             console.log(error)
           })
+        },
+        handleResize() {
+            if (isMobile == true) {
+                this.dimension.height = 280
+                this.dimension.width = 310
+            }else{
+                this.dimension.height = 500
+                this.dimension.width = 1000
+            }
         }
     },
     props: {
@@ -45,6 +62,10 @@
     },
     data() {
       return {
+        dimension :{
+          height: 500,
+          width: 1100,
+        },
         worldData : {},
         lineChartData: {
           labels: Array.from(Array(this.cases.length).keys()),
