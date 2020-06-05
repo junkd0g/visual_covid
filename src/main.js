4d371612c843687e02dfe5e127950a1185254a96
Iordanis Paschalidis <iordanis@ipaschalidis.com>
2020-06-05 10:51:59 +0100

    analytics

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'nprogress/nprogress.css';
 import VueMeta from 'vue-meta'
+import VueAnalytics from 'vue-analytics';
 
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
@@ -14,6 +15,10 @@ Vue.use(VueMeta)
 
 Vue.config.productionTip = false
 
+Vue.use(VueAnalytics, {
+  id: 'UA-168658590-1',
+  router
+})
 
 
 new Vue({
