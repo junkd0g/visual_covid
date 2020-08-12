1384d4987fee02cff6bfd78b00a16a80e09fa936
Iordanis Paschalidis <iordanis@ipaschalidis.com>
2020-08-12 12:40:56 +0100

    removing google analytics

--- src/main.js
+++ src/main.js
@@ -7,7 +7,6 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'nprogress/nprogress.css';
 import VueMeta from 'vue-meta'
-import VueAnalytics from 'vue-analytics';
 
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
@@ -15,10 +14,6 @@ Vue.use(VueMeta)
 
 Vue.config.productionTip = false
 
-Vue.use(VueAnalytics, {
-  id: 'UA-168658590-1',
-  router
-})
 
 
 new Vue({
